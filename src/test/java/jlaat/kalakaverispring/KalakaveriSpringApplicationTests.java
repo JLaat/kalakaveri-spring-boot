69e2f5d72a67beb62133521918ba7bb1a7f94f67
Joni Laatikainen <jonilaatikaine@gmail.com>
2023-01-22 12:27:09 +0200

    Initial commit

---
+++ src/test/java/jlaat/kalakaverispring/KalakaveriSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package jlaat.kalakaverispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KalakaveriSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
