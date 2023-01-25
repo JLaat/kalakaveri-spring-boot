2a3b94c5c0f669533f9738e41da50955444bafc5
Joni Laatikainen <jonilaatikaine@gmail.com>
2023-01-25 10:26:31 +0200

    Created crud methods on all service classes

--- src/main/java/jlaat/kalakaverispring/repository/LakeRepo.java
+++ src/main/java/jlaat/kalakaverispring/repository/LakeRepo.java
@@ -3,7 +3,11 @@ package jlaat.kalakaverispring.repository;
 import jlaat.kalakaverispring.model.Lake;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface LakeRepo extends JpaRepository<Lake, Long> {
+    Optional<Lake> findLakeById(Long id);
+
+    void deleteLakeById(Long id);
 }
