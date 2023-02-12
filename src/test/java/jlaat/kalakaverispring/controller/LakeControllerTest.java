0bda535a9e1b235a03b9bc5c894e14b7701bb565
Joni Laatikainen <jonilaatikaine@gmail.com>
2023-02-12 11:05:44 +0200

    Created tests for controller functions updateLake, and deleteLake

--- src/test/java/jlaat/kalakaverispring/controller/LakeControllerTest.java
+++ src/test/java/jlaat/kalakaverispring/controller/LakeControllerTest.java
@@ -1,17 +1,20 @@
 package jlaat.kalakaverispring.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jlaat.kalakaverispring.model.Lake;
 import jlaat.kalakaverispring.service.LakeService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.core.AutoConfigureCache;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MockMvc.*;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -36,19 +39,49 @@ class LakeControllerTest {
     }
 
     @Test
-    void findLake() {
+    void findLake() throws Exception {
+        Long id = 1L;
+        Lake lake = new Lake(id, "Joensuu", "Aittolampi");
+
+        when(this.lakeService.findLake(1L)).thenReturn(lake);
+
+        this.mockMvc.perform(get("/lake/find/{id}", id))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    void addLake() throws Exception {
+        Lake lake = new Lake(1L, "Joensuu", "Aittolampi");
+
+        when(lakeService.addLake(any(Lake.class))).thenReturn(lake);
+
+        mockMvc.perform(post("/lake/add")
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(lake)))
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
 
     }
 
     @Test
-    void addLake() {
+    void updateLake() throws Exception {
+        Lake lake = new Lake(1L, "Joensuu", "Aittolampi");
+
+        when(lakeService.updateLake(any())).thenReturn(lake);
+
+        MvcResult mvcResult = mockMvc.perform(put("/lake/update")
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(lake)))
+                .andReturn();
+
+        assertEquals(mvcResult.getResponse().getStatus(), 200);
+        assertEquals(mvcResult.getResponse().getContentAsString(), objectMapper.writeValueAsString(lake));
     }
 
     @Test
-    void updateLake() {
-    }
-
-    @Test
-    void deleteLake() {
+    void deleteLake() throws Exception {
+        mockMvc.perform(delete("/lake/delete/{id}", 1L))
+                .andExpect(status().isOk());
     }
 }
